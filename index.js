84f6325cf70f54ca941a61c580a06efd21c76dcf
IgoryanTV0 <avatar200297@gmail.com>
2022-11-26 14:36:48 +0300

    update index.html and index.js

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-var http = require('http');
 var express = require('express');
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: 'remotemysql.com',
+    user: 'xxFhi3fR3w',
+    password: 'ASTO8UWANW',
+    connectionLimit: 5
+});
 
 var app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.json());
 app.use('/img', express.static('web/img'));
 app.use('/css', express.static('web/css'));
 app.use('/js', express.static('web/js'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/web/index.html");
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + "/web/404.html");
 });
 
 app.listen(80);
